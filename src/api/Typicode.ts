23f9d214c937c270ed6811312a29b5f886ef86b2
Timothy Johnson <timothy.johnson@broadcom.com>
2019-09-26 11:17:48 -0400

    Add API and CLI for TypicodeTodos
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>

---
+++ src/api/Typicode.ts
@@ -0,0 +1,29 @@
+/*
+* This program and the accompanying materials are made available under the terms of the
+* Eclipse Public License v2.0 which accompanies this distribution, and is available at
+* https://www.eclipse.org/legal/epl-v20.html
+*
+* SPDX-License-Identifier: EPL-2.0
+*
+* Copyright Contributors to the Zowe Project.
+*
+*/
+
+import { ITodo } from "./doc/Itodo";
+import { RestClient, AbstractSession, ImperativeExpect, Logger } from "@zowe/imperative";
+
+export class Typicode {
+    public static readonly TODO_URI = "/todos";
+
+    public static getTodos(session: AbstractSession): Promise<ITodo[]> {
+        Logger.getAppLogger().trace("Typicode.getTodos() called");
+        return RestClient.getExpectJSON<ITodo[]>(session, Typicode.TODO_URI);
+    }
+
+    public static getTodo(session: AbstractSession, id: number): Promise<ITodo> {
+        Logger.getAppLogger().trace("Typicode.getTodo() called with id " + id);
+        ImperativeExpect.toNotBeNullOrUndefined(id, "id must be provided");
+        const resource = Typicode.TODO_URI + "/" + id;
+        return RestClient.getExpectJSON<ITodo>(session, resource);
+    }
+}
